6f8c09cd95d1db1490f79563570a3e32db0bc97d
AlexksanderUshakov <Privetushki@gmail.com>
2024-10-26 13:01:41 +0500

    Non-Blocking Channel Operations

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+//TimeOut
+
 func main() {
 
 	c1 := make(chan string)
@@ -35,4 +37,43 @@ func main() {
 		fmt.Print("timeout 2")
 	}
 
+	//Non-Blocking Channel Operations
+
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	// Здесь показан неблокирующий приём.
+	// Если значение доступно
+	// в `messages`, то `select` выберет вариант
+	// `<-messages` с этим значением. Если нет,
+	// то будет сразу выбран вариант `default`.
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
+	}
+
+	// Неблокирующая отправка работает аналогично.
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	default:
+		fmt.Println("no message sent")
+	}
+
+	// Можно использовать несколько `case` перед
+	// оператором `default` для реализации многовариантного
+	// неблокирующего выбора. Здесь показана попытка
+	// неблокирующим способом получить
+	// как `messages` так и `signals`.
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
 }
